1f97787e3fbab07692b3ee331fbbdc0ea4c6b253
AudreyPic <audrey.picaud@etu.unilim.fr>
2023-10-10 17:19:53 +0200

    Update UI Creat Event

--- resources/views/events/create_event.blade.php
+++ resources/views/events/create_event.blade.php
@@ -6,33 +6,50 @@
         </div>
     </x-slot>
 
-    <div class="items-center max-w-3xl mx-auto mt-8 p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-        <h1 class="text-2xl font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight mb-6">Créer un Événement</h1>
-        <form action="{{ route('events.store') }}" method="POST">
-            @csrf
-            <div class="mb-4">
-                <label for="name" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Nom de l'Événement</label>
-                <input type="text" class="mt-1 p-2 w-full border rounded-md" id="name" name="name" required>
+    <div class="max-w-4xl mx-auto mt-10 sm:px-6 lg:px-8">
+        <div class="bg-gray-800 dark:bg-gray-900 overflow-hidden shadow-sm sm:rounded-lg text-white">
+            <div class="p-6 border-b border-gray-600">
+                <h2 class="text-2xl font-semibold mb-4">Créer un nouvel événement</h2>
+
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+
+                <form action="{{ route('events.store') }}" method="POST">
+                    @csrf
+
+                    <div class="mb-4">
+                        <label for="nom" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Nom de l'événement</label>
+                        <input type="text" id="name" name="name" class="mt-1 p-2 w-full border rounded dark:bg-gray-900">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="date" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Date</label>
+                        <input type="date" id="date" name="date" class="mt-1 p-2 w-full border rounded dark:bg-gray-900 text-white date-picker">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="heure" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Heure</label>
+                        <input type="time" id="time" name="time" class="mt-1 p-2 w-full border rounded dark:bg-gray-900 text-white time-picker">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="lieu" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Lieu</label>
+                        <input type="text" id="location" name="location" class="mt-1 p-2 w-full border rounded dark:bg-gray-900">
+                    </div>
+
+                    <div class="mb-4">
+                        <label for="description" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Description</label>
+                        <textarea id="description" name="description" rows="3" class="mt-1 p-2 w-full border rounded dark:bg-gray-900"></textarea>
+                    </div>
+
+                    <button type="submit" class="bg-gray-300 text-gray-800 dark:text-gray-200 font-bold py-2 px-4 rounded hover:bg-gray-400">
+                        Créer l'événement
+                    </button>
+                </form>
             </div>
-            <div class="flex mb-4">
-                <div class="mr-2">
-                    <label for="date" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Date</label>
-                    <input type="date" class="mt-1 p-2 w-36 border rounded-md" id="date" name="date" required>
-                </div>
-                <div class="mr-2">
-                    <label for="time" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Heure</label>
-                    <input type="time" class="mt-1 p-2 w-24 border rounded-md" id="time" name="time" required>
-                </div>
-            </div>
-            <div class="mb-4">
-                <label for="location" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Lieu</label>
-                <input type="text" class="mt-1 p-2 w-full border rounded-md" id="location" name="location" required>
-            </div>
-            <div class="mb-6">
-                <label for="description" class="block text-sm font-medium text-gray-600 dark:text-gray-400">Description</label>
-                <textarea class="mt-1 p-2 w-full border rounded-md" id="description" name="description" rows="4" required></textarea>
-            </div>
-            <button type="submit" class="btn-primary px-4 py-2 rounded-md bg-blue-500 text-white hover:bg-blue-700 focus:outline-none focus:ring focus:ring-blue-200">Créer l'Événement</button>
-        </form>
+        </div>
     </div>
 </x-app-layout>
